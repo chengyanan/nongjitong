e94546e4ebb06cf196c0de1a80484e87cd32f87d
chengyanan <1914293706@qq.com>
2016-03-07 18:03:27 +0800

    新闻详情页面下面的工具栏

--- nongjitong/class/discover/news/next Controller/YNBottomToolView.swift
+++ nongjitong/class/discover/news/next Controller/YNBottomToolView.swift
@@ -8,9 +8,139 @@
 
 import UIKit
 
+protocol YNBottomToolViewDelegate {
+
+    func buttonClick(button: UIButton)
+    
+}
+
 class YNBottomToolView: UIView {
 
+    var delegate: YNBottomToolViewDelegate?
     
+    //MARK: life cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.backgroundColor = kRGBA(244, g: 244, b: 244, a: 1)
+        
+        self.addSubview(voteButton)
+        self.addSubview(objectButton)
+        self.addSubview(favoriteButton)
+        self.addSubview(commentButton)
+       
+        setLayout()
+        
+        voteButton.addTarget(self, action: "buttonClick:", forControlEvents: .TouchUpInside)
+        objectButton.addTarget(self, action: "buttonClick:", forControlEvents: .TouchUpInside)
+        favoriteButton.addTarget(self, action: "buttonClick:", forControlEvents: .TouchUpInside)
+        commentButton.addTarget(self, action: "buttonClick:", forControlEvents: .TouchUpInside)
+       
+    }
+    
+    func setLayout() {
+    
+        //voteButton
+        Layout().addLeftConstraint(voteButton, toView: self, multiplier: 1, constant: 0)
+        Layout().addTopConstraint(voteButton, toView: self, multiplier: 1, constant: 0)
+        Layout().addBottomConstraint(voteButton, toView: self, multiplier: 1, constant: 0)
+        
+        //objectButton
+        Layout().addTopBottomConstraints(objectButton, toView: voteButton, multiplier: 1, constant: 0)
+        Layout().addLeftToRightConstraint(objectButton, toView: voteButton, multiplier: 1, constant: 0)
+        Layout().addWidthConstraint(objectButton, toView: voteButton, multiplier: 1, constant: 0)
+        
+        //favoriteButton
+        Layout().addTopBottomConstraints(favoriteButton, toView: objectButton, multiplier: 1, constant: 0)
+        Layout().addLeftToRightConstraint(favoriteButton, toView: objectButton, multiplier: 1, constant: 0)
+        Layout().addWidthConstraint(favoriteButton, toView: objectButton, multiplier: 1, constant: 0)
+        
+        //commentButton
+        Layout().addTopBottomConstraints(commentButton, toView: favoriteButton, multiplier: 1, constant: 0)
+        Layout().addLeftToRightConstraint(commentButton, toView: favoriteButton, multiplier: 1, constant: 0)
+        Layout().addWidthConstraint(commentButton, toView: favoriteButton, multiplier: 1, constant: 0)
+        Layout().addRightConstraint(commentButton, toView: self, multiplier: 1, constant: 0)
+        
+        
+    }
+ 
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    //MARK: event response
+    func buttonClick(sender: UIButton) {
+    
+        
+       self.delegate?.buttonClick(sender)
+        
+    }
+    
+    
+    let voteButton: UIButton = {
+        
+        //赞同
+        let tempView = UIButton()
+        tempView.tag = 1
+        tempView.setTitle("0", forState: .Normal)
+        tempView.setImage(UIImage(named: "agree_up_vote"), forState: .Normal)
+        tempView.translatesAutoresizingMaskIntoConstraints = false
+        tempView.titleLabel?.font = UIFont.systemFontOfSize(11)
+        tempView.setTitleColor(UIColor.grayColor(), forState: .Normal)
+        tempView.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 6)
+        return tempView
+    }()
+    
+    let objectButton: UIButton = {
+        
+        //反对
+        let tempView = UIButton()
+        tempView.tag = 2
+        tempView.setTitle("0", forState: .Normal)
+        tempView.setImage(UIImage(named: "hand_down_vote"), forState: .Normal)
+        tempView.translatesAutoresizingMaskIntoConstraints = false
+        tempView.titleLabel?.font = UIFont.systemFontOfSize(11)
+        tempView.setTitleColor(UIColor.grayColor(), forState: .Normal)
+        tempView.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 6)
+        
+        return tempView
+    }()
+    
+    
+    let favoriteButton: UIButton = {
+        
+        //收藏
+        let tempView = UIButton()
+        tempView.tag = 3
+        tempView.setTitle("收藏", forState: .Normal)
+        tempView.setImage(UIImage(named: "favorite"), forState: .Normal)
+        tempView.translatesAutoresizingMaskIntoConstraints = false
+        tempView.titleLabel?.font = UIFont.systemFontOfSize(11)
+        tempView.setTitleColor(UIColor.grayColor(), forState: .Normal)
+        tempView.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 6)
+        
+        return tempView
+    }()
+    
+    let commentButton: UIButton = {
+        
+        //评论
+        let tempView = UIButton()
+        tempView.tag = 4
+        tempView.setTitle("评论", forState: .Normal)
+        tempView.setImage(UIImage(named: "comment"), forState: .Normal)
+        tempView.translatesAutoresizingMaskIntoConstraints = false
+        tempView.titleLabel?.font = UIFont.systemFontOfSize(11)
+        tempView.setTitleColor(UIColor.grayColor(), forState: .Normal)
+        tempView.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 6)
+        
+        return tempView
+        
+    }()
+
     
     
     
--- nongjitong/class/discover/news/next Controller/YNNewsDetailsViewController.swift
+++ nongjitong/class/discover/news/next Controller/YNNewsDetailsViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class YNNewsDetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class YNNewsDetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, YNBottomToolViewDelegate, YNInputViewDelegate {
     
     var newsModel: YNNewsModel?
     var model: YNNewsDetailsModel?
     
     
     var tableView: UITableView?
+    
+    var bottomView: YNBottomToolView?
+    
+    var messageStr = ""
+    var isKeyboardShowing = false
+    var keyBoardHeight: CGFloat?
+    //inputView的高度
+    let inputViewHeight: CGFloat = 44
+    
+    //添加一个随键盘弹出的view
+    var bottomInputView: YNInputView?
+    
+    var cellHeight: CGFloat {
+        
+        var cellHeight: CGFloat = 0
+        
+        if let _ = self.model {
+        
+            cellHeight = 44 + 30 + self.model!.contentHeight
+            
+            for item in self.model!.comments {
+                
+                cellHeight += item.height
+            }
+            
+            cellHeight += CGFloat(self.model!.relation.count) * 44
+            
+        }
+      
+        return cellHeight
+    }
+    
     
     
     //MARK: life circle
@@ -41,6 +73,23 @@ class YNNewsDetailsViewController: UIViewController, UITableViewDataSource, UITa
         //加载数据
         getNewsDetail()
     }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        addKeyBoardNotication()
+        
+        
+    }
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        removeKeyBoardNotication()
+    }
+    
+    deinit {
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
     
     //MARK: 设置界面
     func setupInterface() {
@@ -55,6 +104,23 @@ class YNNewsDetailsViewController: UIViewController, UITableViewDataSource, UITa
         self.view.addSubview(tempTableView)
         self.tableView = tempTableView
         
+        let tgr = UITapGestureRecognizer(target: self, action: "hideKeyBoard")
+        
+        self.tableView?.addGestureRecognizer(tgr)
+        
+        
+        let tempinputView = YNInputView()
+        tempinputView.delegate = self
+        tempinputView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(tempinputView)
+        self.bottomInputView = tempinputView
+        
+        
+        self.bottomView = YNBottomToolView()
+        self.bottomView?.delegate = self
+        self.view.addSubview(bottomView!)
+        
+       
     }
     
     func setLayout() {
@@ -63,8 +129,19 @@ class YNNewsDetailsViewController: UIViewController, UITableViewDataSource, UITa
         Layout().addTopConstraint(tableView!, toView: self.view, multiplier: 1, constant: 0)
         Layout().addLeftConstraint(tableView!, toView: self.view, multiplier: 1, constant: 0)
         Layout().addRightConstraint(tableView!, toView: self.view, multiplier: 1, constant: 0)
-        Layout().addBottomConstraint(tableView!, toView: self.view, multiplier: 1, constant: 0)
+        Layout().addBottomConstraint(tableView!, toView: self.view, multiplier: 1, constant: -44)
         
+        //bottomView
+        Layout().addLeftConstraint(bottomView!, toView: self.view, multiplier: 1, constant: 0)
+        Layout().addRightConstraint(bottomView!, toView: self.view, multiplier: 1, constant: 0)
+        Layout().addBottomConstraint(bottomView!, toView: self.view, multiplier: 1, constant: 0)
+        Layout().addHeightConstraint(bottomView!, toView: nil, multiplier: 0, constant: 44)
+        
+        //inputView
+        Layout().addBottomConstraint(bottomInputView!, toView: self.view, multiplier: 1, constant:0)
+        Layout().addHeightConstraint(bottomInputView!, toView: nil, multiplier: 0, constant: inputViewHeight)
+        Layout().addLeftConstraint(bottomInputView!, toView: self.view, multiplier: 1, constant: 0)
+        Layout().addRightConstraint(bottomInputView!, toView: self.view, multiplier: 1, constant: 0)
         
     }
     
@@ -288,6 +365,243 @@ class YNNewsDetailsViewController: UIViewController, UITableViewDataSource, UITa
         }
         
     }
+   
+    
+    //MARK: YNBottomToolViewDelegate
+    func buttonClick(button: UIButton) {
+        
+        switch button.tag {
+        
+        case 1:
+            
+            //TODO: 赞成
+            
+            break
+        case 2:
+            
+            //反对
+            self.newsCommentVoteOppose()            
+            
+            break
+            
+        case 3:
+            
+            //TODO:收藏
+            
+            break
+        case 4:
+            
+            //评论
+            self.bottomInputView?.textView.becomeFirstResponder()
+            
+            break
+        default:
+            break
+            
+        }
+        
+    }
+    
+    
+    
+    
+   
+    //MARK: keyBoardNotication
+    func addKeyBoardNotication() {
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func removeKeyBoardNotication() {
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+        
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        
+        if let userInfo = notification.userInfo {
+            
+            self.isKeyboardShowing = true
+            
+            let keyboardBounds = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+            
+            let duration = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+            
+            let keyboardBoundsRect = self.view.convertRect(keyboardBounds, toView: nil)
+            
+            let deltaY = keyboardBoundsRect.size.height
+            
+            self.keyBoardHeight = deltaY
+            
+            let needScroolHeight = cellHeight + deltaY + inputViewHeight + 64 - self.view.frame.size.height
+            
+            let animations: (()->Void) = {
+                
+                self.bottomInputView!.transform = CGAffineTransformMakeTranslation(0, -deltaY)
+                
+                if needScroolHeight > 0 {
+                    
+                    if needScroolHeight  > deltaY {
+                        
+                        self.tableView?.transform = CGAffineTransformMakeTranslation(0, -deltaY)
+                        
+                    } else {
+                        
+                        self.tableView?.transform = CGAffineTransformMakeTranslation(0, -needScroolHeight)
+                    }
+                    
+                }
+                
+                
+            }
+            
+            if duration > 0 {
+                
+                let options = UIViewAnimationOptions(rawValue: UInt((userInfo[UIKeyboardAnimationCurveUserInfoKey] as! NSNumber).integerValue << 16))
+                
+                UIView.animateWithDuration(duration, delay: 0, options:options, animations: animations, completion: nil)
+                
+            } else {
+                
+                animations()
+            }
+            
+        }
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        
+        if let userInfo = notification.userInfo {
+            
+            self.isKeyboardShowing = false
+            
+            let duration = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSNumber).doubleValue
+            let animations:(() -> Void) = {
+                
+                self.bottomInputView!.transform = CGAffineTransformIdentity
+                self.tableView!.transform = CGAffineTransformIdentity
+            }
+            
+            if duration > 0 {
+                
+                let options = UIViewAnimationOptions(rawValue: UInt((userInfo[UIKeyboardAnimationCurveUserInfoKey] as! NSNumber).integerValue << 16))
+                UIView.animateWithDuration(duration, delay: 0, options:options, animations: animations, completion: nil)
+                
+            } else{
+                
+                animations()
+            }
+        }
+        
+    }
+    
+    
+    //MARK: YNInputViewDelegate
+    func inputViewFinishButtonDidClick() {
+        
+        if self.messageStr == "" {
+            
+            
+        } else {
+            
+            //TODO: 有内容发送
+            
+            
+            //判断tableView是否需要向上移动
+//            decideIfTableViewNeedMoveUp()
+            
+            self.messageStr = ""
+            
+            //TODO: 判断移动到评论区
+            
+            self.view.endEditing(true)
+        }
+        
+        
+    }
+    
+    //判断tableView是否需要向上移动
+    func inputViewTextViewDidChange(text: String) {
+        
+        self.messageStr = text
+    }
+    
+    func decideIfTableViewNeedMoveUp() {
+        
+        if isKeyboardShowing {
+            
+            //当有键盘的时候点击发送
+            ifTableViewNeedScrollWithKeyboard()
+            
+        } else {
+            
+            //当没键盘的时候点击发送
+            ifTableViewNeedScrollWithoutKeyboard()
+            
+        }
+        
+        
+    }
+    
+    func ifTableViewNeedScrollWithoutKeyboard() {
+        
+        let tableViewNeedScrollHeight = 64 + cellHeight + inputViewHeight - self.view.frame.size.height
+        
+        if tableViewNeedScrollHeight > 0 {
+            
+            //需要滚动
+            self.tableView?.scrollToRowAtIndexPath(NSIndexPath(forRow: self.model!.comments.count - 1, inSection: 1), atScrollPosition: UITableViewScrollPosition.Bottom, animated: true)
+            
+            
+        } else {
+            
+            //如果cell高度没有超出tableview的高度 不需要滚动
+        }
+    }
+    
+    func ifTableViewNeedScrollWithKeyboard() {
+        
+        let needScroolHeight = 64 + cellHeight + self.keyBoardHeight! + inputViewHeight - self.view.frame.size.height
+        
+        let animations: (()->Void) = {
+            
+            if needScroolHeight > 0 {
+                
+                //最高的滚动高度
+                let maxScrollHeight = self.keyBoardHeight!
+                
+                if needScroolHeight > self.keyBoardHeight! {
+                    //如果需要向上移动的距离大于键盘的距离， 就向下移动tableview
+                    self.tableView?.scrollToRowAtIndexPath(NSIndexPath(forRow: self.model!.comments.count - 1, inSection: 0), atScrollPosition: UITableViewScrollPosition.Bottom, animated: true)
+                    
+                    self.tableView?.transform = CGAffineTransformMakeTranslation(0, -maxScrollHeight)
+                    
+                } else {
+                    
+                    //如果需要向上移动的距离小于键盘的距离， 就向上移动tableview
+                    self.tableView?.transform = CGAffineTransformMakeTranslation(0, -needScroolHeight)
+                }
+                
+                
+            }
+            
+        }
+        
+        animations()
+        
+    }
+    
+    
+    func hideKeyBoard() {
+        
+        self.view.endEditing(true)
+    }
+    
+    
     
     //MARK:加载新闻详情
     func getNewsDetail() {
@@ -349,13 +663,67 @@ class YNNewsDetailsViewController: UIViewController, UITableViewDataSource, UITa
     }
     
     
-    
-    func setAttributeString() {
-    
+    //MARK: 反对某条新闻评论／取消反对
+    func newsCommentVoteOppose() {
+        
+        let params: [String: String?] = ["m": "Appapi",
+            "key": "KSECE20XE15DKIEX3",
+            "c": "NewsCommentVote",
+            "a": "oppose",
+            "comment_id": newsModel?.id,
+            "user_id": kUser_ID() as? String
+        ]
+        
+        let progress = YNProgressHUD().showWaitingToView(self.view)
+        
+        Network.post(kURL, params: params, success: { (data, response, error) -> Void in
+            
+            progress.hideUsingAnimation()
+            
+            do {
+                
+                let json: NSDictionary =  try NSJSONSerialization.JSONObjectWithData(data , options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                
+                //                print("data - \(json)")
+                
+                if let status = json["status"] as? Int {
+                    
+                    if status == 1 {
+                        
+                        //TODO: 成功刷新界面
+                        
+                        
+                        
+                    } else if status == 0 {
+                        
+                        if let msg = json["msg"] as? String {
+                            
+                            //                            print(msg)
+                            YNProgressHUD().showText(msg, toView: self.view)
+                        }
+                    }
+                    
+                }
+                
+                
+            } catch {
+                
+                YNProgressHUD().showText("请求错误", toView: self.view)
+            }
+            
+            
+            }) { (error) -> Void in
+                
+                progress.hideUsingAnimation()
+                YNProgressHUD().showText("加载失败", toView: self.view)
+        }
+        
+        
         
     }
-    
-    
-    
-    
+
+
+
+
+
 }
