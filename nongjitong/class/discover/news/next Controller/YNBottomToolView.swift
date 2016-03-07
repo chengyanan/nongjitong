a34b82d158fb8424b61a0e79dc6351b610f13260
chengyanan <1914293706@qq.com>
2016-03-07 10:39:33 +0800

    添加一个底部工具栏

---
+++ nongjitong/class/discover/news/next Controller/YNBottomToolView.swift
@@ -0,0 +1,18 @@
+//
+//  YNBottomToolView.swift
+//  nongjitong
+//
+//  Created by 农盟 on 16/3/7.
+//  Copyright © 2016年 农盟. All rights reserved.
+//
+
+import UIKit
+
+class YNBottomToolView: UIView {
+
+    
+    
+    
+    
+    
+}
