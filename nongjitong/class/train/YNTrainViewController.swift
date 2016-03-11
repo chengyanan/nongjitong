1af199c2d026134a017f4e53679d0018d4a0808e
chengyanan <1914293706@qq.com>
2016-03-11 17:08:34 +0800

    视频

---
+++ nongjitong/class/train/YNTrainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  YNTrainViewController.swift
+//  nongjitong
+//
+//  Created by 农盟 on 16/3/11.
+//  Copyright © 2016年 农盟. All rights reserved.
+//
+
+import UIKit
+
+class YNTrainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
